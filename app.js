61b7185708f7efeed100028c69186ea890df271c
elizabethMiranda <mirandaeli23@gmail.com>
2023-11-10 16:47:23 -0600

    quitando extensiones a nombre de paginas

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ app.get('/hola-mundo', (req, res) => {
   res.send('Hello mundo en su respectiva');
 });
 
+app.get('/generic', (req, res) => {
+  res.sendFile(__dirname + '/public/generic.html');
+});
+
+app.get('/elements', (req, res) => {
+  res.sendFile(__dirname + '/public/elements.html');
+});
+
 app.get('*', (req, res) => {
   res.sendFile(__dirname + '/public/404.html');
 })
